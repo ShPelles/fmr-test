2a7b0be2fc7a7f1ed6dd7e39d9c3c4d9200ef13a
Shalom <shalom.pelles@gmail.com>
2022-01-06 14:55:38 +0200

    fix

--- src/app/_shared/market-data.service.ts
+++ src/app/_shared/market-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter, interval, of, switchMap, tap } from 'rxjs';
+import { filter, interval, map, of, switchMap, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { StockValue } from './models/stock-value.model';
 import { StocksListService } from './stocks-list.service';
@@ -13,6 +13,7 @@ const INTERVAL = 1000;
 export class MarketDataService {
 
   private lastUpdateId = 0;
+  private lastUpdate: StockValue[] = [];
 
   constructor(
     private stocksListService: StocksListService,
@@ -23,10 +24,13 @@ export class MarketDataService {
     return interval(INTERVAL).pipe(
       switchMap(() => this.stocksListService.getList()),
       switchMap(symbols => symbols.length > 0 ?
-        this.http.get<StockValue[]>(environment.apiUrl + 1, { params: { symbols: symbols.toString() } }) :
+        this.http.get<StockValue[]>(environment.apiUrl + 1, { params: { symbols: symbols.toString() } }).pipe(
+          tap(result => { if (result.length > 0) this.lastUpdateId = result[0].UpdateId }),
+          map(result => symbols.map(symbol => result.find(r => r.Symbol === symbol) || this.lastUpdate.find(r => r.Symbol === symbol)!)),
+        ) :
         of([])
       ),
-      tap(result => this.lastUpdateId = result[0]?.UpdateId || 0),
+      // tap(result => this.lastUpdateId = result[0]?.UpdateId || 0),
     )
   }
 }
